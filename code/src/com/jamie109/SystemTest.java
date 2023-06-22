d44b5a828f0b0cc9ee2133e9a36161083f46cd4f
jamie109 <2356408976@qq.com>
2023-06-22 18:20:28 +0800

    finish v1

--- code/src/com/jamie109/SystemTest.java
+++ code/src/com/jamie109/SystemTest.java
@@ -24,8 +24,10 @@ public class SystemTest {
                     stuList = deleteStu(stuList);
                     break;
                 case 3:
+                    stuList = updateStu(stuList);
                     break;
                 case 4:
+                    searchStu(stuList);
                     break;
                 case 5:
                     isExit = true;
@@ -84,5 +86,41 @@ public class SystemTest {
         }
         System.out.println("ERROR：未找到该学生id");
         return list;
+    }
+    public static ArrayList<Student> updateStu(ArrayList<Student> list){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("输入要修改信息的学生id");
+        int id = sc.nextInt();
+        for(int i = 0; i < list.size(); i++ ){
+            if(list.get(i).getId() == id){
+                System.out.printf("find it!");
+                System.out.print("input name: ");
+                String name = sc.next();
+                System.out.print("input age: ");
+                int age = sc.nextInt();
+                System.out.print("input home address: ");
+                String homeAddr = sc.next();
+                list.get(i).setName(name);
+                list.get(i).setAge(age);
+                list.get(i).setHomeAddr(homeAddr);
+                return list;
+            }
+        }
+        System.out.println("没有该学生，请重新输入");
+        return list;
+    }
+    public static void searchStu(ArrayList<Student> list){
+//        Scanner sc = new Scanner(System.in);
+        System.out.println("输入要查询的学生id");
+//        int id = sc.nextInt();
+        if(list.size() == 0)
+            System.out.println("当前无学生信息，请添加后在查询");
+        else{
+            System.out.printf("id\t\t name\t\t age\t homeAddr\t\t\n");
+            for(int i = 0; i < list.size(); i++){
+                Student s = list.get(i);
+                System.out.printf("%s\t %s\t %s\t %s\t\n", s.getId(), s.getName(), s.getAge(),s.getHomeAddr());
+            }
+        }
     }
 }
